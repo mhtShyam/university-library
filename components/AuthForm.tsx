d18c6bf749f7640e3284feb0a41a6696fe4d7f3e
shyam <shyammht3@gmail.com>
2025-04-09 11:04:51 +0530

    first commit

---
+++ components/AuthForm.tsx
@@ -0,0 +1,14 @@
+
+
+interface Props {
+    type: string;
+    schema: any;
+    defaultValues: any;
+    onSubmit: any;
+}
+const AuthForm = ({type, schema, defaultValues, onSubmit}:Props) => {
+    return (
+        <div>AuthForm</div>
+    )
+}
+export default AuthForm
